6f60d842d7427d90b28fb9d8cb366dd887651480
Darius Ronkowitz <dronkowitz@yahoo.com>
2025-10-30 13:34:50 -0400

    `Add scripts to enable new features.
    Added scripts from Vivetool to enable new 25H2 features. I also added a bat script and a text file.

---
+++ Enable New 25H2 Features!.bat
@@ -0,0 +1,20 @@
+:: Start Menu
+vivetool /enable /id:46079892 
+vivetool /enable /id:42916428 
+vivetool /enable /id:44788892
+
+:: File Explorer
+vivetool /enable /id:40729001 
+vivetool /enable /id:42105254 
+
+:: Taskbar & System Tray
+vivetool /enable /id:42169223 
+vivetool /enable /id:43132439 
+
+:: Copilot+ / Search Enhancements
+vivetool /enable /id:41655236 
+vivetool /enable /id:44773875 
+
+:: Misc UI Tweaks
+vivetool /enable /id:46320257 
+vivetool /enable /id:44571814 
---
+++ Enable New 25H2 Features!.cs
@@ -0,0 +1,25 @@
+// These features are used with the new version of Windows 11 25H2/24H2
+public class Windows 11 Features: Menu;
+
+
+public bool setupNewFeatures = true;
+::Start Menu
+vivetool /enable /id:46079892 
+vivetool /enable /id:42916428 
+vivetool /enable /id:44788892
+
+:: File Explorer
+vivetool /enable /id:40729001 
+vivetool /enable /id:42105254 
+
+:: Taskbar & System Tray
+vivetool /enable /id:42169223 
+vivetool /enable /id:43132439 
+
+:: Copilot+ / Search Enhancements
+vivetool /enable /id:41655236 
+vivetool /enable /id:44773875 
+
+:: Misc UI Tweaks
+vivetool /enable /id:46320257 
+vivetool /enable /id:44571814 
